503fd8e67c805c85000a0d6904dc26e39cd04004
Madison Dailey <mdailey034@halifax.edu>
2026-01-18 14:39:02 -0500

    STRUCTURE & PSEUDOCODE

---
+++ Week3-example.py
@@ -0,0 +1,10 @@
+# Import Statements
+
+# Function Definitions
+
+# Program Logic
+# Assign values to variable: name, goal, birth_year, and current_year
+# Calculate age
+# Print top border
+# Print bio sentence
+# Print bottom border
